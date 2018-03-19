046865c89d71c0b09e20fa2ec46afe71b7caa8b7
behrami <behram1993@gmail.com>
2018-03-19 12:50:26 -0400

    finished exercise2

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+puts "How would you calculate a good tip for a 55 dollar meal?"
+puts 55 * 13.0/100
+
+puts "string" + 5.to_s
+
+puts "45628 \* 7839 =  #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
